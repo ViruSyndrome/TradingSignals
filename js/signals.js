69f72887e6fd0d493ccd8fa73f357b2c99072604
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-08-18 01:14:06 +0530

    UI: Move moonshot scan status to the left side of the header to perfectly balance the layout without rigid widths

--- js/signals.js
+++ js/signals.js
@@ -668,7 +668,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.7',
+  _version: '5.8',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
