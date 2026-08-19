f219cd7f8fd4f4e5fd7c64b0b95c9581a0a0a91f
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-08-20 01:37:22 +0530

    chore: bump app version to 5.13 to bust cache for lock icons

--- js/signals.js
+++ js/signals.js
@@ -773,7 +773,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.9',
+  _version: '5.13',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
