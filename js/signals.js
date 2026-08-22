6f179bb24104d714a49a88fe7604a5721ff034f1
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-08-22 15:50:10 +0530

    chore: bump version to 5.24

--- js/signals.js
+++ js/signals.js
@@ -773,7 +773,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.23',
+  _version: '5.24',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
