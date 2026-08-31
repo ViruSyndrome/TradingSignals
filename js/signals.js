3c46b73037e7e65e169efb5f169f5976ccbae31f
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-08-31 15:59:57 +0530

    fix: swap stale CryptoPanic widget for TradingView real-time breaking news timeline

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.47',
+  _version: '5.48',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
