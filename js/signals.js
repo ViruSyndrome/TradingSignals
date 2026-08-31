d34b3d3836dddd5add3d6e0657cd360e6648d1e6
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-01 01:58:38 +0530

    style: extract transparent icon from logo, use sharp HTML text for brand, and convert to clickable home button

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.54',
+  _version: '5.55',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
