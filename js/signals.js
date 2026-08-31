47fc99fd81afca69710369441bcf4cafeb1c55a0
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-08-31 08:35:29 +0530

    fix: bump master app version string to forcefully wipe browser cache

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.30',
+  _version: '5.41',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
