f6cec1e3883ef9f13de580452f0afeb6e5e9e5e1
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-01 23:46:29 +0530

    polish: mobile summary bar 3-col compact mode, moonshot empty state UX, branded update splash screen (v5.61)

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.60',
+  _version: '5.61',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
