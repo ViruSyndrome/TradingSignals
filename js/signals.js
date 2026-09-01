07e645d13a8e4476b8681d565eaca0e8551a108c
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-01 22:55:00 +0530

    fix: update loader text to crypto-only, bump SW cache v3, sync all versions to 5.60

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.59',
+  _version: '5.60',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
