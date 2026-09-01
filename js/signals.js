e20d8760d3107d9f9b40e40172c36830a2c78875
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-01 18:03:40 +0530

    docs: add real-world trading strategy cards to Learn Trading tab (OCO, 8-day hold, P2P)

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.57',
+  _version: '5.58',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
