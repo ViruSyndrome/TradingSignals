70c7d3f02fbebc589fad48036c9488fdea350c7a
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-04 18:39:07 +0530

    Document dashboard holdings and backtest workflow

--- js/signals.js
+++ js/signals.js
@@ -779,7 +779,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.72',
+  _version: '5.73',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'trendrunner-cache-v15';
+﻿const CACHE_NAME = 'trendrunner-cache-v16';
 
 self.addEventListener('message', event => {
   if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
