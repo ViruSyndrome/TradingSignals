c321b01b7a24b373ca3caaf3a2facae4700e2210
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-04 18:44:59 +0530

    Fix tracked assets heading and signal log layout

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'trendrunner-cache-v16';
+﻿const CACHE_NAME = 'trendrunner-cache-v17';
 
 self.addEventListener('message', event => {
   if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
