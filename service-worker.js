3d64a4dc9ea34a99739c9ca3ea6f31fe1f76003f
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-06 03:51:50 +0530

    Refresh social preview image metadata

--- service-worker.js
+++ service-worker.js
@@ -1,4 +1,4 @@
-﻿const CACHE_NAME = 'trendrunner-cache-v36';
+﻿const CACHE_NAME = 'trendrunner-cache-v37';
 
 self.addEventListener('message', event => {
   if (event.data?.type === 'SKIP_WAITING') self.skipWaiting();
