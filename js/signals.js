43a17134e80885e9193e6cd314c4376c33a3bb89
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-07 18:05:21 +0530

    ui: slow down news ticker animation speed to 60s

--- js/signals.js
+++ js/signals.js
@@ -786,7 +786,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '6.00',
+  _version: '6.01',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
