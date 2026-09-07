d37cd26903dacc22a5be53bd4090d3b485dafff0
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-07 13:07:14 +0530

    chore: bump version to 6.00 to force cache clear

--- js/signals.js
+++ js/signals.js
@@ -786,7 +786,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '5.96',
+  _version: '6.00',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
