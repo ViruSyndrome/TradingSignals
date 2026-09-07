f64701853a49a51dae8fd352177b1210e2a700b8
ViruSyndrome <vinodjamesisaac@gmail.com>
2026-09-07 19:16:59 +0530

    chore: bump global version to 6.05

--- js/signals.js
+++ js/signals.js
@@ -786,7 +786,7 @@ const Signals = {
     return this.LEVELS[signalKey] || this.LEVELS.NEUTRAL;
   },
 
-  _version: '6.01',
+  _version: '6.05',
 };
 
 if (typeof module !== 'undefined' && module.exports) module.exports = Signals;
